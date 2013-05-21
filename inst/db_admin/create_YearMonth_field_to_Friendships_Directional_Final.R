85a02635b7e531836d7b05a4daea7e0d8b96802f
Ilmo van der Lowe <github@vanderlowe.com>
2013-05-21 13:20:52 +0100

    Updated to include column creation

--- inst/db_admin/create_YearMonth_field_to_Friendships_Directional_Final.R
+++ inst/db_admin/create_YearMonth_field_to_Friendships_Directional_Final.R
@@ -1,3 +1,6 @@
 require(magic)
 # Create YearMonth field to be used as a quick lookup for data ranges
+magicSQL("ALTER TABLE Friendships_Directional_Final 
+         ADD YearMonth VARCHAR(6)", "cpw_meta")
+
 magicSQL("UPDATE Friendships_Directional_Final SET YearMonth = concat(cast(Year as char), cast(LPAD(Month, 2, '0') as char))", "cpw_meta")
