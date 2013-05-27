53718e6b72b3285c18d489a59acf943d05ca9442
Ilmo van der Lowe <github@vanderlowe.com>
2013-05-27 12:08:28 +0100

    Fixed iso2 being used a variable and function name

--- R/plot.crises.R
+++ R/plot.crises.R
@@ -13,20 +13,19 @@
 plot.crises <- function(country) {
   country <- toupper(as.character(country))
   # Get country codes
-  iso2s <- as.character(getISOs(2))
-  iso3s <- as.character(getISOs(3))
+  all.isos <- c(as.character(getISOs(2)), as.character(getISOs(3)))
   
   if (country %in% c(iso2s, iso3s)) {
     # Get both 2- and 3-letter ISO codes
-    if (nchar(country) == 2) {iso2 <- country; iso3 <- iso3(country)}
-    if (nchar(country) == 3) {iso2 <- iso2(country); iso3 <- country}
+    if (nchar(country) == 2) {iso2.code <- country; iso3.code <- iso3(country)}
+    if (nchar(country) == 3) {iso2.code <- iso2(country); iso3.code <- country}
     
-    events <- magicSQL(sprintf("SELECT year, month, title as name FROM emergencies WHERE ISOalpha3 = '%s' AND month IS NOT NULL AND year > 2005", iso3), "cpw_Crises")
+    events <- magicSQL(sprintf("SELECT year, month, title as name FROM emergencies WHERE ISOalpha3 = '%s' AND month IS NOT NULL AND year > 2005", iso3.code), "cpw_Crises")
     events$date <- paste(events$year, events$month, 15)
     events$year <- NULL
     events$month <- NULL
     
-    plot.facebook(iso2, events)
+    plot.facebook(iso2.code, events)
     
   } else {
     stop("Please provide valid 2- or 3-letter ISO country code.")
