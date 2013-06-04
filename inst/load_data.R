ba9186d73ae32c84141bfbd0b6527474a54a3f09
Ilmo van der Lowe <github@vanderlowe.com>
2013-06-04 12:18:06 +0100

    Correct path

--- inst/load_data.R
+++ inst/load_data.R
@@ -5,5 +5,5 @@ require(testthat)
 data.files <- list.files(system.file("extdata", package="spellbook"))
 
 for (f in data.files) {
-  source(f)
+  source(system.file("extdata", f, package="spellbook"))
 }
