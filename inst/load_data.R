9a21bdde8d5f3033b6536b7a893f4d74d950b5b5
Ilmo van der Lowe <github@vanderlowe.com>
2013-06-04 11:40:02 +0100

    Renamed to 'extdata' due to reserved folder name 'data'

--- inst/load_data.R
+++ inst/load_data.R
@@ -2,7 +2,7 @@ require(magic)
 require(data.table)
 require(testthat)
 
-data.files <- list.files(system.file("data", package="spellbook"))
+data.files <- list.files(system.file("extdata", package="spellbook"))
 
 for (f in data.files) {
   source(f)
