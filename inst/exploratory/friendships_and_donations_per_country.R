d98ef00b7ff64cab9a5c23d47f82aa5771f7dc8d
Ilmo van der Lowe <github@vanderlowe.com>
2013-06-14 10:24:45 +0100

    Streamlining; added tests for data integrity

--- inst/exploratory/friendships_and_donations_per_country.R
+++ inst/exploratory/friendships_and_donations_per_country.R
@@ -4,19 +4,22 @@ Sys.setlocale(locale="C")
 
 # Step 1: Get basic crisis data
 loadData("crises")
-
-# Step 2a: Get Facebook friendship data for the month of the crisis; friendships from donors
 crisis.ids <- unique(crises$emergency_id)
 
-incoming.friendships <- sapply(crisis.ids, getGlobalIncomingFacebookRelationsByDisaster)
+# Step 2: Get Facebook friendship data for the month of the crisis
+incoming.friendships <- lapply(crisis.ids, getGlobalIncomingFacebookRelationsByDisaster)
 incoming.friendships <- do.call(rbind, incoming.friendships)
 incoming.friendships <- data.table(incoming.friendships)
-incoming.friendships <- incoming.friendships[, list(emergency_id, target_country = Friended_Country, source_country = Friender_Country, friendships = Count, year = Year, month = Month)]
 setkeyv(incoming.friendships, c("emergency_id", "target_country", "source_country", "year", "month"))
 
+test_that("The incoming friendship data is of expected size", {
+  emergency.count <- length(crisis.ids)
+  country.count <- nrow(allCountries())
+  expect_equal(nrow(incoming.friendships), country.count * emergency.count)
+})
+
 # Calculate total number of friendships per country per month
-total.friendships.sums <- incoming.friendships[, list(total_friendships = sum(friendships)), by = list(year, month, target_country)]
-setkeyv(total.friendships.sums, c("target_country", "year", "month"))
+incoming.friendships[, total_friendships := sum(friendships), by = list(year, month, target_country)]
 
 # Step 3: Get donations by donor countries
 getDonations <- function(id) {
@@ -25,7 +28,7 @@ getDonations <- function(id) {
   return(magicSQL(sql, "cpw_Crises"))
 }
 
-donations <- sapply(crisis.ids, getDonations)
+donations <- lapply(crisis.ids, getDonations)
 donations <- do.call(rbind, donations)
 donations <- data.table(donations)
 donations <- donations[, list(emergency_id, source_country = donorISO, amount)]
@@ -37,24 +40,16 @@ donations.by.country <- donations[, list(total_amount = sum(amount)), by = sourc
 # Step 4: Merge data
 # Merge friendships with donations
 friendships.with.donations <- merge(incoming.friendships, donations, by = c("emergency_id", "source_country"), all.x = T, all.y = T)
-setkeyv(crises.with.donations, c("emergency_id", "target_country", "year", "month", "source_country"))
+setkeyv(friendships.with.donations, c("emergency_id", "target_country", "year", "month", "source_country"))
+test_that("The data is of expected size", {
+  expect_equal(nrow(incoming.friendships), nrow(friendships.with.donations))
+})
 
-# Merge crises with friendships
-crises.with.friendships <- na.omit(merge(crises, incoming.friendships, by = c("emergency_id", "target_country", "year", "month"), all.x = T, all.y = T))
+# Merge crises with friendships and donations data
+crises <- merge(crises, friendships.with.donations, by = c("emergency_id", "target_country", "year", "month"), all.x = T, all.y = T)
 setkeyv(crises.with.friendships, c("emergency_id", "target_country", "year", "month", "source_country"))
 
-# Merge friendships and donors
-crises.with.friendships.and.donations <- merge(crises.with.donations, crises.with.friendships, all.x = T, all.y = T)
-
-all <- merge(na.omit(crises.with.friendships), na.omit(donations), by = c("emergency_id", "donor_country"), all.x = T, all.y = T)
-all <- merge(all, total.friendships.sums, by = c("target_country", "year", "month"), all.x = T, all.y = T)
-donor_friendships <- all[, list(donor_friendships = sum(friendships)), by = emergency_id]
-all <- merge(all, donor_friendships, by = "emergency_id")
-
 # Step 5: Calculate percentages
-all$funding_percent <- all$amount / all$total_funding  # Funding by all donor entities (incl. NGOs and private organizations)
-all$friend_percent <- all$friendships / all$total_friendships  # Aggregating all global friendship ties
-all$donor_friend_percent <- all$friendships / all$donor_friendships  # Aggregating all global friendship ties
 
 # Step 6: National level data
 country.data.timeseries <- data.table(magicSQL("SELECT Code AS country, Year, GDP, GDP_PPP, InternetUsers, Population FROM countries_by_year", "cpw_meta"))
